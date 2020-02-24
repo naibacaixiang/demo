61e975149145e84454d76b550ac548accdfc1384
alinaiba <zuicc@qq.com>
2020-02-24 07:40:20 +0000

    一些基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1>guanyu  about页</h1>
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel5555555555555
                 </div>
 
                 <div class="links">
